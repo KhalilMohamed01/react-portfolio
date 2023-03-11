e1d6ca27898b360499507e546ac1a6730962cd9e
KHALIL Mohamed <m3d.khalil01@gmail.com>
2023-03-11 16:47:11 +0100

    Working on Header Component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import './header.css'
+import {CTA} from './CTA'
+import ME from '../../assets/me.png'
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>KHALIL Mohamed</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
+        <div className="me">
+            <img src={ME} alt="" />
+        </div>
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+    </div>
   )
 }
