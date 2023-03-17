49bc4253a2c38eb7045ac96fd9c78b9685d0d2c8
KHALIL Mohamed <m3d.khalil01@gmail.com>
2023-03-17 23:41:26 +0100

    experience component dev

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 export const Experience = () => {
   return (
@@ -11,11 +12,64 @@ export const Experience = () => {
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            <article className="experience__details"></article>
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <h4>HTML</h4>
+              <small class="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <h4>CSS</h4>
+              <small class="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <h4>JAVASCRIPT</h4>
+              <small class="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <h4>VUEJS</h4>
+              <small class="text-light">Beginner</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <h4>REACTJS</h4>
+              <small class="text-light">Beginner</small>
+            </article>
           </div>
         </div>
       </div>
-      <div className="experience__backend"></div>
+      <div className="experience__backend">
+      <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <h4>HTML</h4>
+              <small class="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <h4>CSS</h4>
+              <small class="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <h4>JAVASCRIPT</h4>
+              <small class="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <h4>VUEJS</h4>
+              <small class="text-light">Beginner</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill/>
+              <h4>REACTJS</h4>
+              <small class="text-light">Beginner</small>
+            </article>
+          </div>
+      </div>
             </section>
   )
 }
