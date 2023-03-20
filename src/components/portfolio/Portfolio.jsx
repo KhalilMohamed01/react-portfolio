86270e3408919de136a73156ec9b71786d155ad7
KHALIL Mohamed <m3d.khalil01@gmail.com>
2023-03-20 23:24:08 +0100

    Creating portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
 export const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <a href="http://github.com" target="_blank" className='btn'>Github</a>
+            <a href="http://dribbble.com" target="_blank" className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <a href="http://github.com" target="_blank" className='btn'>Github</a>
+            <a href="http://dribbble.com" target="_blank" className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <a href="http://github.com" target="_blank" className='btn'>Github</a>
+            <a href="http://dribbble.com" target="_blank" className='btn btn-primary'>Live Demo</a>
+        </article>
+      </div>
+
+
+    </section>
   )
 }
