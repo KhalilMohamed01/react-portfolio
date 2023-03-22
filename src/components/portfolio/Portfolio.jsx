08fa8fbe3e4339a1d9570ddc14adde4ca9eb5e52
KHALIL Mohamed <m3d.khalil01@gmail.com>
2023-03-22 23:17:44 +0100

    Creating portfolio array

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,6 +6,51 @@ import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
+const data =[
+  {
+    id:1,
+    image:IMG1,
+    title:'This is a portfolio item title',
+    github:'http://github.com',
+    demo:'http://dribbble.com'
+  },
+  {
+    id:2,
+    image:IMG2,
+    title:'This is a portfolio item title',
+    github:'http://github.com',
+    demo:'http://dribbble.com'
+  },
+  {
+    id:3,
+    image:IMG3,
+    title:'This is a portfolio item title',
+    github:'http://github.com',
+    demo:'http://dribbble.com'
+  },
+  {
+    id:4,
+    image:IMG4,
+    title:'This is a portfolio item title',
+    github:'http://github.com',
+    demo:'http://dribbble.com'
+  },
+  {
+    id:5,
+    image:IMG5,
+    title:'This is a portfolio item title',
+    github:'http://github.com',
+    demo:'http://dribbble.com'
+  },
+  {
+    id:6,
+    image:IMG6,
+    title:'This is a portfolio item title',
+    github:'http://github.com',
+    demo:'http://dribbble.com'
+  }
+]
+
 export const Portfolio = () => {
   return (
     <section id="portfolio">
