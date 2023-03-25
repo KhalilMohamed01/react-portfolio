438b2238ce9bff160ca61d86cb879cc04df73c64
KHALIL Mohamed <m3d.khalil01@gmail.com>
2023-03-25 01:15:14 +0100

    working on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <a href="#" className='footer__logo'>PORTFOLIO</a>
+
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#potfolio">Potfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://facebook.com"><FaFacebookF/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://twitter.com"><IoLogoTwitter/></a>
+
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; PORTFOLIO. All rights reserved</small>
+      </div>
+    </footer>
+    )
 }
